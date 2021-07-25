9858c1f4e2a06e8becad56f26cf38c04635881e0
pranjaldas <pranjaldas525@gmail.com>
2021-07-25 17:47:49 +0530

    one question done

--- RandomQuestions/RemoveDuplicate.java
+++ RandomQuestions/RemoveDuplicate.java
@@ -1,8 +1,8 @@
 // Write a program to remove duplicates from a unsorted array.
 import java.util.*;
 class RemoveDuplicate{
-    public static void main(String[] args){
-        int arr[]= {1,4,5,5,1,3,9,9,10,3};
+    public static void method1(int[] arr){
+        // My first approach
         ArrayList<Integer> duplicates = new ArrayList<Integer>();
 
         for(int i=0; i<arr.length-1; i++) {
@@ -17,7 +17,21 @@ class RemoveDuplicate{
                 System.out.print(arr[i]);
             }
         }
-        System.out.println(duplicates);
+    }
+    public static void method2(int[] arr){
+        ArrayList<Integer> duplicates = new ArrayList<Integer>();
+        for(int i=0; i<arr.length;i++){
+            if(!duplicates.contains(arr[i]))
+                System.out.print(" "+arr[i]);
+            duplicates.add(arr[i]);
+        }
+
+    }
+    public static void main(String[] args){
+        int arr[]= {1,4,5,5,1,3,9,9,10,3};
+        method1(arr);
+        method2(arr);
+       
 
     }
 }
