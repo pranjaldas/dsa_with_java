91598767bed57c4c2dd6f5c63c89a0e2004a2b22
pranjaldas <pranjaldas525@gmail.com>
2022-02-19 08:32:15 +0530

    ok Arrays are going on

---
+++ Arrays/Array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+// STRING IS THE ARRAY OF CHAR
+// IN STRING WE USE "", AND IN CASE OF CHAR WE USE ''
+public class Array {
+    public static final void main(String[] args) {
+        char[] arr;
+        arr = new char[3];
+        arr[0] ='A';
+        arr[1] ='B';
+
+        char[] array= {'a', 'b', 'c'};
+
+        System.out.println("Array is : "+Arrays.toString(array));
+    }
+}
