5648647245009942b3b4ab4b4423205b11dbd05a
pranjaldas <pranjaldas525@gmail.com>
2022-05-09 07:34:46 +0530

    hamap revice

--- CollectionsFramework/src/HashMapTest.java
+++ CollectionsFramework/src/HashMapTest.java
@@ -1,21 +1,26 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 /**
  * ONE NULL KEY AND MULTIPLE NULL VALUE
  * */
 public class HashMapTest {
     public static void main(String[] args){
         Map<Integer,String > hmap= new HashMap<>();
-        hmap.put(1,"Pranjal");
-        hmap.put(2,"Vinod");
-        hmap.put(3,"hii");
+        hmap.put(116,"Pranjal");
         hmap.put(null,"int");
         hmap.put(null,"hola");
-        hmap.put(4,null);
+        hmap.put(46,null);
         hmap.put(5,null);
+        hmap.put(28,"Vinod");
+        hmap.put(30,"hii");
         System.out.println(hmap.put(1,"new value"));
         hmap.forEach((key,value)->{
             System.out.println("key: "+key+" value: "+value);
         });
+        System.out.println("---------new iteration-------");
+        Set<Integer> keys= hmap.keySet();
+        Iterator iterator= keys.iterator();
+        while(iterator.hasNext())
+            System.out.println(hmap.get(iterator.next()));
     }
 }
