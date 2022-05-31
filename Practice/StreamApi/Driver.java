7695c9c94187a94a1532232107de6e7908f537f8
pranjaldas <pranjaldas525@gmail.com>
2022-05-31 19:55:19 +0530

    Stream api going good

--- Practice/StreamApi/Driver.java
+++ Practice/StreamApi/Driver.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 public class Driver {
@@ -10,9 +12,22 @@ public class Driver {
         students.add(new Student("Vinod",5638));
         students.add(new Student("Amlan",908));
         // students.forEach(i->System.out.println(i.toString()));
+        Comparator<Student> comparator= (Student a, Student b)->{
+            if(a.getSalary()>b.getSalary())
+                return 1;
+            else
+                return -1;
+        };
+        Collections.sort(students,comparator);
+        students.forEach(i->System.out.println(i.toString()));
+        // -------------------------Find Max or Min using Stream------------------------
         Student student=students.stream()
                 .max(Comparator.comparing(Student::getSalary))
                 .orElseThrow(NoSuchElementException::new);
-        System.out.println(student.toString());
+        System.out.println("With heighst salary: "+student.toString());
+
+        // -------------------------Filter a list based on a given value using Stream------------------------
+        List<Student> list=students.stream().filter((s)->s.getSalary()>1000).collect(Collectors.toList());
+        list.forEach(i->System.out.println(i.toString()));
     }
 }
