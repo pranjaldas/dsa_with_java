f1eeaef38cd3f9ae7f9d971e53759fec7bd62d4d
pranjaldas <pranjaldas525@gmail.com>
2022-06-29 10:35:17 +0530

    JS practice

---
+++ js/AsyncAwait.js
@@ -0,0 +1,35 @@
+
+const data=[
+    {
+        name: "Pranjal Das",
+        age:27
+    },
+
+    {
+        name: "Druna Baruah",
+        age:22
+    }
+]
+function getDatas() {
+    console.log(data);
+}
+const createData =()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            data.push({
+                name: "Utpal Da",
+                age:24
+            });
+            let error= false;
+            if(!error)
+                resolve("data");
+            else
+                reject("Error occured");
+        },1000);
+    }); 
+}
+async function start(){
+    await createData();
+    getDatas();
+}
+start();
